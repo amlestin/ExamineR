0fe9a5b24d7fdfee13d5e14bb245dfc8e685de78
Arnold Lestin <amlestin@gmail.com>
2018-08-22 10:13:46 -0400

    Fixed open report directory function for macOS

--- ExamineR.R
+++ ExamineR.R
@@ -101,7 +101,7 @@ opendir <- function(dir = getwd()) {
   if (.Platform['OS.type'] == "windows") {
     shell.exec(dir)
   } else {
-    system(paste(Sys.getenv("R_BROWSER"), dir))
+    system("open .")
   }
 }
 
