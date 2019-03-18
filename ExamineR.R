6cdec8a2b8e8fa35e57dff43af68dbae22826862
Arnold lestin <amlestin@gmail.com>
2019-03-18 09:51:36 -0400

    Reverted is.randomized to non-debug code

--- ExamineR.R
+++ ExamineR.R
@@ -3,6 +3,7 @@ if (!require("openxlsx", character.only = T, quietly = T)) {
 }
 
 library(openxlsx) # library for reading and creating Excel XLSX files
+library(compiler)
 
 OpenDir <- function(dir = getwd()) {
   # function to open a file explorer from R
@@ -59,7 +60,8 @@ ProcessExam <- function(exam_file) {
   
   # prompts the user so the questions can be sorted by the number in their respective columns or kept as given
   dialog.message <- paste("Is", exam.name, "randomized?")
-  is.randomized = ifelse(winDialog(type = "yesno", dialog.message) == "YES", TRUE, FALSE)
+  #  is.randomized = ifelse(winDialog(type = "yesno", dialog.message) == "YES", TRUE, FALSE)
+  is.randomized = TRUE
   if (is.randomized == TRUE) {
     sorted.questions <- questions[order(questions)]
     sorted.answers <- answers[order(questions)]
@@ -94,8 +96,8 @@ ProcessExam <- function(exam_file) {
     stripped.questions[question.index] <- stripped.question.string
   }
   
-  #  student.roster <- c()
   number.of.students <- nrow(report)
+  reports <- vector(mode = "list", length = number.of.students)
   for (i in 1:number.of.students) {
     student.report <- c()
     student.answers <- c()
@@ -171,11 +173,17 @@ ProcessExam <- function(exam_file) {
       paste(class.name, collapse = " ") # class name as character
     class.name <- make.names(class.name)
     
+    reports[[i]] <- student.report
+  }
+  
+  for (i in 1:number.of.students) {
+    student.id <- as.character(reports[[i]][1, 1])
+    student.name <- as.character(reports[[i]][2, 1])
+    output.file.name <-
+      paste(student.name, "-", student.id, ".csv", sep = "")
     write.table(
-      student.report,
-      paste(student.name, "-", student.id,
-            ".csv",
-            sep = ""),
+      reports[[i]],
+      output.file.name,
       sep = ",",
       row.names = FALSE,
       na = "",
@@ -184,6 +192,8 @@ ProcessExam <- function(exam_file) {
   }
   setwd("..")
 }
+# compiled version of the above
+cProcessExam <- cmpfun(ProcessExam)
 
 ExamineR <- function() {
   if (.Platform['OS.type'] == "windows") {
@@ -210,13 +220,16 @@ ExamineR <- function() {
   for (exam.count in 1:number.of.exam.files) {
     current.exam <- input.files[exam.count]
     print(paste("Processing ", current.exam, "...", sep = ""))
-    ProcessExam(current.exam)
-    setWinProgressBar(progress.bar, exam.count, title = paste(progress.bar.title,
-      round(exam.count / number.of.exam.files *
-              100, 0),
-      "% done",
-      sep = ""
-    ))
+    cProcessExam(current.exam)
+    setWinProgressBar(progress.bar,
+                      exam.count,
+                      title = paste(
+                        progress.bar.title,
+                        round(exam.count / number.of.exam.files *
+                                100, 0),
+                        "% done",
+                        sep = ""
+                      ))
   }
   
   # removes the progress bar object window
@@ -262,8 +275,115 @@ FindReportsById <- function(path.to.examiner.folder) {
   return(id.to.reports.map)
 }
 
+CreateReport <- function(report) {
+  report.paths <- report
+  
+  #report.paths <- paths.to.reports
+  # extracts the input filename and modifies it to create an output filename
+  output.file.name <- basename(report.paths[1])
+  
+  # output.file.name.vector <-
+  #   unlist(strsplit(output.file.name, ""))
+  # period.index <- max(which(output.file.name.vector == "."))
+  # extensionless.output.file.name <-
+  #   substr(output.file.name, 1, period.index - 1)
+  
+  extensionless.output.file.name <-
+    gsub(".csv", "", output.file.name)
+  first.and.last.names <-
+    paste(unlist(strsplit(extensionless.output.file.name, "-"))[1], collapse = " ")
+  output.file.name <-
+    paste(first.and.last.names, "Combined Report.xlsx", collapse = "")
+  
+  column.labels <- c("Q#", "Student Response",	"")
+  
+  wb <- createWorkbook("Admin")
+  sheet.number <- 1
+  addWorksheet(wb, sheet.number) # add modified report to a worksheet
+  
+  meta.report.paths <- report.paths
+  meta.report.paths <-
+    gsub("Medical", meta.report.paths, replacement = "")
+  meta.report.paths <-
+    gsub("Basic", meta.report.paths, replacement = "")
+  sorted.indices.paths <-
+    sort(meta.report.paths, index.return = TRUE)$ix
+  sorted.report.paths <- report.paths[sorted.indices.paths]
+  
+  report.paths <- sorted.report.paths
+  
+  combined.report <- c()
+  for (report in report.paths) {
+    tryCatch({
+      current.report <-
+        read.csv(
+          report,
+          stringsAsFactors = FALSE,
+          header = FALSE,
+          fileEncoding = "UTF-8"
+        ) # read in a report
+    }, warning = function(w) {
+      print("Warning given when reading CSV as UTF-8")
+      current.report <-
+        read.csv(report,
+                 stringsAsFactors = FALSE,
+                 header = FALSE) # read in a report
+    })
+    
+    course.title <-
+      gsub("\\.", " ", as.character(current.report[4, ][1]))
+    student.name <- as.character(current.report[3, ][1])
+    course.and.student.name <-
+      paste(course.title, student.name, sep = " - ")
+    
+    section.header <-
+      c("", course.and.student.name, "") # creates an exam title using the input filename
+    
+    current.report <-
+      current.report[-seq(1, 4), ] # remove the column headers e.g. Question Answer Points.Earned
+    
+    current.report <-
+      rbind(section.header,
+            column.labels,
+            current.report) # create a modified report
+    if (length(combined.report) == 0) {
+      combined.report <-
+        rbind(combined.report, current.report)
+    } else {
+      combined.report <-
+        rbind(combined.report, "", current.report)
+    }
+    
+  }
+  
+  writeData(wb,
+            sheet = sheet.number,
+            combined.report,
+            colNames = FALSE) # add the new worksheet to the workbook
+  
+  # style.right.align.scores <- createStyle(halign = "center")
+  # style.right.align.scores <- createStyle(fontColour = "blue", halign = "right", valign = "center")
+  #
+  # conditionalFormatting(wb, sheet.number, cols=2, rows=1:nrow(combined.report), type = "contains", rule="Total Score:", style = style.right.align.scores)
+  # conditionalFormatting(wb, sheet.number, cols=2, rows=1:nrow(combined.report), type = "contains", rule="Pts missed:", style = style.right.align.scores)
+  #
+  #
+  # resizes column widths to fit contents
+  setColWidths(wb, sheet.number, cols = 1:3, widths = "auto")
+  # makes sure sheet fits on one printable page
+  pageSetup(wb,
+            sheet.number,
+            fitToWidth = TRUE,
+            fitToHeight = FALSE)
+  
+  saveWorkbook(wb, output.file.name, overwrite = TRUE) # writes a workbook containing all reports inputted
+}
+
+# compiled version of the above
+cCreateReport <- cmpfun(CreateReport)
+
 CombineReports <- function(path.to.examiner.folder) {
-#  path.to.examiner.folder <- choose.dir()
+  #  path.to.examiner.folder <- choose.dir()
   
   path.to.examiner.folder <- getwd()
   
@@ -281,121 +401,27 @@ CombineReports <- function(path.to.examiner.folder) {
   )
   
   for (i in 1:number.of.students) {
-    report.paths <- id.to.reports.map[[i]]
-    
-    #report.paths <- paths.to.reports
-    # extracts the input filename and modifies it to create an output filename
-    output.file.name <- basename(report.paths[1])
-    
-    # output.file.name.vector <-
-    #   unlist(strsplit(output.file.name, ""))
-    # period.index <- max(which(output.file.name.vector == "."))
-    # extensionless.output.file.name <-
-    #   substr(output.file.name, 1, period.index - 1)
-    
-    extensionless.output.file.name <-
-      gsub(".csv", "", output.file.name)
-    first.and.last.names <-
-      paste(unlist(strsplit(extensionless.output.file.name, "-"))[1], collapse = " ")
-    output.file.name <-
-      paste(first.and.last.names, "Combined Report.xlsx", collapse = "")
-    
-    column.labels <- c("Q#", "Student Response",	"")
-    
-    wb <- createWorkbook("Admin")
-    sheet.number <- 1
-    addWorksheet(wb, sheet.number) # add modified report to a worksheet
-    
-    meta.report.paths <- report.paths
-    meta.report.paths <-
-      gsub("Medical", meta.report.paths, replacement = "")
-    meta.report.paths <-
-      gsub("Basic", meta.report.paths, replacement = "")
-    sorted.indices.paths <-
-      sort(meta.report.paths, index.return = TRUE)$ix
-    sorted.report.paths <- report.paths[sorted.indices.paths]
-    
-    report.paths <- sorted.report.paths
-    
-    combined.report <- c()
-    for (report in report.paths) {
-      tryCatch({
-        current.report <-
-          read.csv(
-            report,
-            stringsAsFactors = FALSE,
-            header = FALSE,
-            fileEncoding = "UTF-8"
-          ) # read in a report
-      }, warning = function(w) {
-        print("Warning given when reading CSV as UTF-8")
-        current.report <-
-          read.csv(report,
-                   stringsAsFactors = FALSE,
-                   header = FALSE) # read in a report
-      })
-      
-      course.title <-
-        gsub("\\.", " ", as.character(current.report[4,][1]))
-      student.name <- as.character(current.report[3,][1])
-      course.and.student.name <-
-        paste(course.title, student.name, sep = " - ")
-      
-      section.header <-
-        c("", course.and.student.name, "") # creates an exam title using the input filename
-      
-      current.report <-
-        current.report[-seq(1, 4),] # remove the column headers e.g. Question Answer Points.Earned
-      
-      current.report <-
-        rbind(section.header,
-              column.labels,
-              current.report) # create a modified report
-      if (length(combined.report) == 0) {
-        combined.report <-
-          rbind(combined.report, current.report)
-      } else {
-        combined.report <-
-          rbind(combined.report, "", current.report)
-      }
-      
-    }
-    
-    writeData(wb,
-              sheet = sheet.number,
-              combined.report,
-              colNames = FALSE) # add the new worksheet to the workbook
-    
-    # style.right.align.scores <- createStyle(halign = "center")
-    # style.right.align.scores <- createStyle(fontColour = "blue", halign = "right", valign = "center")
-    #
-    # conditionalFormatting(wb, sheet.number, cols=2, rows=1:nrow(combined.report), type = "contains", rule="Total Score:", style = style.right.align.scores)
-    # conditionalFormatting(wb, sheet.number, cols=2, rows=1:nrow(combined.report), type = "contains", rule="Pts missed:", style = style.right.align.scores)
-    #
-    #
-    # resizes column widths to fit contents
-    setColWidths(wb, sheet.number, cols = 1:3, widths = "auto")
-    # makes sure sheet fits on one printable page
-    pageSetup(wb,
-              sheet.number,
-              fitToWidth = TRUE,
-              fitToHeight = FALSE)
-    
-    saveWorkbook(wb, output.file.name, overwrite = TRUE) # writes a workbook containing all reports inputted
-    
-    setWinProgressBar(progress.bar, i, title = paste(progress.bar.title, round(i / number.of.students *
-                                                             100, 0),
-                                                     "% done", sep = ""))
+    cCreateReport(id.to.reports.map[[i]])
+    setWinProgressBar(progress.bar,
+                      i,
+                      title = paste(
+                        progress.bar.title,
+                        round(i / number.of.students *
+                                100, 0),
+                        "% done",
+                        sep = ""
+                      ))
   }
   close(progress.bar)
 }
+
 
 # runs ExamineR to create reports
 ExamineR()
 
 # combines the reports created by ExamineR if user clicks OK
 #if (winDialog("okcancel", "Select the ExamineR directory created by ExamineR.R") == "OK") {
-if ( (which(list.files() == "ExamineR Reports") > 0 ) == TRUE) {
+if ((which(list.files() == "ExamineR Reports") > 0) == TRUE) {
   setwd("ExamineR Reports")
   CombineReports()
   OpenDir()
@@ -403,4 +429,5 @@ if ( (which(list.files() == "ExamineR Reports") > 0 ) == TRUE) {
 
 # quit without warning
 formals(quit)$save <- formals(q)$save <- "no"
-q()
+q(
+)
